c06dbe3f638d391ab541173bc9e328d9fd94b369
rscottyh <rscottyh.dev@gmail.com>
2022-12-06 10:39:08 -0500

    minor editorial changes

--- displayTbl_connection.py
+++ displayTbl_connection.py
@@ -3,13 +3,13 @@ import sqlite3
 def connect():
 
     try:
-        con1 = sqlite3.connect("C:/Tools/hands-on-sql/test2.db")
+        con1 = sqlite3.connect("C:/Tools/hands-on-sql/test.db")
 
     #cur1 = con1.cursor()
 
     #cur1.execute(
     #    "CREATE TABLE IF NOT EXISTS customers(id INTEGER PRIMARY KEY, First TEXT, Surname TEXT)")
-        print("SQLite connection successful!")
+        print("\nConnection to SQLite successful!\n")
     except sqlite3.Error as err:
         print(f"Error: '{err}'")
 
--- displayTbl_main.py
+++ displayTbl_main.py
@@ -5,7 +5,7 @@ import tkinter as tk
 import sqlite3
 
 import displayTbl_connection
-import displayTbl_view
+#import displayTbl_view
 
 # connect to the database
 displayTbl_connection.connect()
