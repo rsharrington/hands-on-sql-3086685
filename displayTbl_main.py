f76ee0740b0e7198447af8fa6e5f152c05118d80
rscottyh <rscottyh.dev@gmail.com>
2022-12-06 10:41:28 -0500

    name change

--- displayTbl_main.py
+++ displayTbl_main.py
@@ -4,8 +4,8 @@ import tkinter as tk
 
 import sqlite3
 
-import displayTbl_connection
+import sqliteDb_connection
 #import displayTbl_view
 
 # connect to the database
-displayTbl_connection.connect()
+sqliteDb_connection.connect()
